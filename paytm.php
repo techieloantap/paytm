e6f4e4cd095303eed35378ede0627ba251b23779
vaibhav <vaibhavk32@gmail.com>
2021-03-08 11:05:13 +0530

    veify signature function added

--- paytm.php
+++ paytm.php
@@ -2,6 +2,7 @@
 
 namespace aw2\paytm;
 
+require_once("vendor/paytm/paytmchecksum/PaytmChecksum.php");
 
 \aw2_library::add_service('paytm.generate_signature','',['namespace'=>__NAMESPACE__]);
 
@@ -29,6 +30,30 @@ function generate_signature($atts,$content=null,$shortcode){
 	$checksum = PaytmChecksum::generateSignature(json_encode($request_body, JSON_UNESCAPED_SLASHES), $merchant_key);
 
 	return array("status"=>"success","signature"=>$checksum);
+}
+
+function verify_signature($atts,$content=null,$shortcode){
+	
+	if(\aw2_library::pre_actions('all',$atts,$content,$shortcode)==false)return;
+	extract( shortcode_atts( array(
+	'request_body'=>'',
+	'merchant_key'=>'',
+	'signature'=>''
+	), $atts) );
+	
+	$input_res=check_required_input($atts);
+	if(isset($input_res['status']) && $input_res['status']==='error'){
+		return $input_res;
+	}
+	
+	$verify_signature = PaytmChecksum::verifySignature(json_encode($request_body,JSON_UNESCAPED_SLASHES), $merchant_key,$signature );
+	
+	if($verify_signature){
+		return array('status'=>'success','message signature is correct.');
+	}
+	
+	return array('status'=>'error','message signature is incorrect.');
+	
 }
 
 /*
